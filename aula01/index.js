86196fe16554227ebb17cc99d19b52e1139afec2
T0madon <joao.daciuk@gmail.com>
2025-02-27 10:40:03 -0300

    primeira aula

---
+++ aula01/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+// import http from "http";
+
+// function handleRequest(req, res) {
+//   if (req.method === "GET" && req.url === "/aula01") {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.write(`<h1>${req.method}</h1>`);
+//     res.end();
+//   }
+// }
+
+// const server = http.createServer(handleRequest);
+// server.listen(3000);
+// console.log("Server running at http://localhost:3000/");
